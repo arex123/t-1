58148afaf6e3c74b09a809dcaac370d1c853d185
arex123 <ad47kumar@gmail.com>
2024-08-02 15:00:19 +0530

    created server with some endpoints

--- index.js
+++ index.js
@@ -5,6 +5,24 @@ const http = require('http')
 
 const server = http.createServer((req,res)=>{
     console.log("Hello from server")
+
+    console.log(req.url)
+
+    res.setHeader("content-type","text/html")
+    if(req.url=="/home"){
+        res.write("<html> <body><p>Welcome home</p></body> </html>")
+        
+    }else if(req.url=="/about"){
+        res.write("<html> <body><p>Welcome to About Us page</p></body> </html>")
+        
+    }else if(req.url=="/node"){
+        
+        res.write("<html> <body><p>Welcome to my Node Js project</p></body> </html>")
+    }
+
+    res.end()
+
+
 })
 
 server.listen(5001,()=>{
