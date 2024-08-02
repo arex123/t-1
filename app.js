cef5b46d99b5f982f9fdde7304bc531602561419
arex123 <ad47kumar@gmail.com>
2024-08-02 23:12:27 +0530

    form submit,middleware,post

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const http = require('http')
 const express = require('express')
+const bodyparser = require('body-parser')
 
 const app = express()
+app.use(bodyparser.urlencoded({extended:false}))
 
-app.use((req,res,next)=>{
-    console.log("first middleware")
-    next()
+app.use('/add-product',(req,res)=>{
+    res.send('<html><body><form action="/product" method="POST"><input name="movie" type="text" placeholder="movie"/><input name="size" type="number" placeholder="enter size" /><button type="submit">submit</button></form></body></html>')
 })
 
-app.use((req,res,next)=>{
-    console.log("second middleware")
-    res.send( { "key1": "value" })
+app.post('/product',(req,res)=>{
+    console.log("body data")
+    console.log(req.body)
+    res.redirect('/')
 })
 
+app.use('/',(req,res)=>{
+    res.send("form submitted")
+})
 
 
 app.listen(5001)
