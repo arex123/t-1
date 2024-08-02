5fb006fc32949d69f97c6f9ce4abc65b9a5de2ef
arex123 <ad47kumar@gmail.com>
2024-08-02 11:37:06 +0530

    sever using http

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log("hello world")
+
+//creating a new sever
+const http = require('http')
+
+const server = http.createServer((req,res)=>{
+    console.log("Hello from server")
+})
+
+server.listen(5001,()=>{
+    console.log("server is running")
+})
