20a34d7b2e95544de3f7a0b6af90fb5b08bdeee9
arex123 <ad47kumar@gmail.com>
2024-08-03 15:59:05 +0530

    views servered perfectly

--- routes/user.js
+++ routes/user.js
@@ -5,15 +5,11 @@ const path = require('path')
 
 // /admin/add-product
 router.get('/login',(req,res)=>{
-    // res.sendFile(path.join(__dirname,'../','views','login.html'))
-    res.send('<form onsubmit="localStorage.setItem(`username`, document.getElementById(`username`).value)" action="/login" method="POST"><input id="username" type="text" name="title"><button type="submit">add</button></form>')
+    res.sendFile(path.join(__dirname,'../','views','login.html'))
 })
 
 router.post('/login',(req,res)=>{
-    // localStorage.setItem("username",req.body.name)
-    // console.log('subbmti ng fo: ',req.body)
-    res.redirect('/app/chat')
-    
+    res.redirect('/app/chat')    
 })
 
 
